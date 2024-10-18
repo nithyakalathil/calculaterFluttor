607f667827f2c909ece9511e557dd3941d0fe5dc
nithyakalathil <nithya913k@gmail.com>
2024-10-18 15:17:30 +0530

    created

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(Calculator());
+}
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: ,
+      ),
+    );
+  }
+}
